18f702814317a1233c59fef9d582f1995915e97a
canlas1 <dc@uscr.com>
2017-07-21 15:43:11 -0400

    got Sass working with create-react-app

---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+ import {
+   Panel,
+   Button,
+   FormControl,
+   Checkbox,
+   Radio,
+   InputGroup,
+   Form
+
+ } from 'react-bootstrap';
+
+
+
+// import FormControlFeedback from 'react-bootstrap/lib/FormControlFeedback';
+// import FormControlStatic from 'react-bootstrap/lib/FormControlStatic';
+// import InputGroupAddon from 'react-bootstrap/lib/InputGroupAddon';
+
+// const loginSchema = new Schema({
+  
+//   login:{
+//     type: String, 
+//     required: true
+//   },
+
+//   password:{
+//     type: String, 
+//     required: true
+//   } 
+// });
+
+class Login extends React.Component {
+  render(){
+    return(
+
+      <div>
+        
+        <div className="Login">
+          <h1>Login Portal!</h1>
+     
+      </div>
+     </div>
+      )
+  }
+}
+
+export default Login
