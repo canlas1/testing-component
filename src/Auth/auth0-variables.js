77bcb1380b0d38f6b0985db530eb2ac95e5f6fd8
canlas1 <dc@uscr.com>
2017-07-24 03:32:41 -0400

    localhost vs heroku

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,12 @@
 export const AUTH_CONFIG = {
   domain: 'auththis.auth0.com',
   clientId: '49zmmUdiwD3dbEz4tqaWqjVVxalSRAeT',
-  callbackUrl: 'https://energy-logan-testing-component.herokuapp.com/callback',
+  callbackUrl: 'http://localhost:3000/callback',
   apiUrl: 'http://auththis.com'
 }
+
+
+//AUTH0_CALLBACK_URL
+//AUTH0_CLIENT_ID
+//AUTH0_CLIENT_SECRET
+//   callbackUrl: 'https://energy-logan-react.herokuapp.com/callback',
