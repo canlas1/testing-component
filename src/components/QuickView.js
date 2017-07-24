a66e7797569733393c02f02df877893b6e7a6db5
lasperilla <l.asperilla@gmail.com>
2017-07-23 20:13:17 -0400

    commit for heroku

--- src/components/QuickView.js
+++ src/components/QuickView.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-import {Collapse} from 'pui-react-collapse';
+// import {Collapse} from 'pui-react-collapse';
 import '../App.css';
-
-
 
 class QuickView extends Component {
     constructor(props) {
@@ -11,18 +9,7 @@ class QuickView extends Component {
 	render() {
 		return (
 			<div className="QuickViewDiv">
-			<Collapse header="Click to view more" divider>
-				{(this.props.data) ? (
-					<div>
-						<p>Customer: {this.props.data.customer}</p>
-						<p>Address: {this.props.data.address}</p>
-						<p>Contact Name: {this.props.data.contact_name}</p>
-						<p>Contact Number: {this.props.data.contact_number}</p>
-						<p>Account Number: {this.props.data.account_number}</p>
-					</div>
-					): <p>No data</p>
-				}
-			</Collapse>
+
 			</div>
 		)
 	}
